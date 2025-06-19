7ec9d46a8038b3e31f2a8711bd8c7f372dcf5d94
Hubert Gabrys <hubert.gabrys@gmail.com>
2025-06-19 11:28:28 +0200

    First try at docs

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Z-Rad'
+copyright = '2025, USZ Medical Physics'
+author = 'USZ Medical Physics'
+release = '24.4'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#005f73",
+        "color-brand-content": "#0a9396",
+    },
+}
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. Z-Rad documentation master file, created by
+   sphinx-quickstart on Thu Apr 17 23:53:54 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Z-Rad documentation
+===================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: zrad.filtering.filtering_definitions
+    :members:
